48c7dcea7bae07e7110789836c7c8478e24c2912
pcaressa <paolo@caressa.it>
2024-04-02 16:12:21 +0200

    Inserimento nel repository del materiale per il modello a 3

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PIA-2022
 Code repository for project PIA 2022 - Expert System
+
+# Moise Friedman model
--- src/model/temp.py
+++ src/model/temp.py
@@ -1,2 +1,28 @@
 # Example of use of pde library
 
+from pde import PDEBase, ScalarField, UnitGrid
+
+from pde import CartesianGrid, VectorField
+
+grid = CartesianGrid([[-2, 2], [-2, 2]], 32)
+field = VectorField.from_expression(grid, ["sin(x)", "cos(x)"])
+field.plot(method="streamplot", title="Stream plot")
+
+class KuramotoSivashinskyPDE(PDEBase):
+    """Implementation of the normalized Kuramoto–Sivashinsky equation"""
+
+    def evolution_rate(self, state, t=0):
+        """implement the python version of the evolution equation"""
+        state_lap = state.laplace(bc="auto_periodic_neumann")
+        state_lap2 = state_lap.laplace(bc="auto_periodic_neumann")
+        state_grad = state.gradient(bc="auto_periodic_neumann")
+        return -state_grad.to_scalar("squared_sum") / 2 - state_lap - state_lap2
+
+
+grid = UnitGrid([32, 32])  # generate grid
+state = ScalarField.random_uniform(grid)  # generate initial condition
+
+eq = KuramotoSivashinskyPDE()  # define the pde
+result = eq.solve(state, t_range=10, dt=0.01)
+result.plot()
+
